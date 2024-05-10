af6d013444b6edbdf0d63ffbd4ae00f647da0524
Alan <alan-hd@hotmail.com>
2024-05-10 09:33:56 -0300

    componentes criados

---
+++ src/components/navBar/NavBar.tsx
@@ -0,0 +1,27 @@
+import { Link, useNavigate } from 'react-router-dom'
+
+
+
+function Navbar() {
+
+
+
+    return (
+        <>
+            <div className='w-full bg-cyan-600 text-white flex justify-center py-4'>
+                <div className="container flex justify-between text-lg">
+                    <div className='text-2xl font-bold uppercase'>Farmacia</div>
+
+                    <div className='flex gap-4'>
+                        <Link to='/home' className='hover:underline'>Home</Link>
+                        <Link to='/categoria' className='hover:underline'>Categoria</Link>
+
+
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Navbar
---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,30 @@
+
+import './Home.css';
+
+function Home() {
+    return (
+        <>
+        <div className=" bg-cyan-600 flex justify-center">
+          <div className='container grid grid-cols-2 text-white'>
+            <div className="flex flex-col gap-4 items-center justify-center py-4">
+              <h2 className='text-5xl font-bold'>Seja bem vindos!</h2>
+              <p className='text-xl'>Confira aqui os remedios</p>
+  
+              <div className="flex justify-around gap-4">
+              
+                {/* <button className='rounded bg-white text-blue-800 py-2 px-4'>Ver jogos</button> */}
+              </div>
+            </div>
+  
+            <div className="flex justify-center ">
+             
+      
+            </div>
+          </div>
+        </div>
+        
+      </>
+    );
+}
+
+export default Home;
