83a261d31f288a07365fcd8c6de3d658a15c682a
Alan <alan-hd@hotmail.com>
2024-05-10 12:00:31 -0300

    correção Home

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -8,7 +8,7 @@ function Home() {
           <div className='container grid grid-cols-2 text-white'>
             <div className="flex flex-col gap-4 items-center justify-center py-4">
               <h2 className='text-5xl font-bold'>Seja bem vindos!</h2>
-              <p className='text-xl'>Cofira seus produtos aqui!</p>
+              <p className='text-xl'>Confira seus produtos aqui!</p>
   
               <div className="flex justify-around gap-4">
               
